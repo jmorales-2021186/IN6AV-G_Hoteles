80b9d632a957c81fa6e081791a6f9c0bf4d5e115
mchajon-2021205 <mchajon-2021205@kinal.edu.gt>
2023-05-08 15:59:57 -0600

    Se agrego hero

--- gestor-de-hoteles-react/src/App.jsx
+++ gestor-de-hoteles-react/src/App.jsx
@@ -9,6 +9,8 @@ export const App = ()=> {
   return (
     <>
       <Outlet/>
+
+      
     </>
   )
 }
--- gestor-de-hoteles-react/src/pages/HomePage.jsx
+++ gestor-de-hoteles-react/src/pages/HomePage.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { NavBar } from '../components/NavBar'
 import '../HomePage.css'
 
 export const HomePage = () => {
   return (
     <>
-      <div id='general'>
-        <NavBar />
+
+      <NavBar />
+
+      <div className='hero'>
+        <div className='reservar'>
+            <Link className='reservar-button'> Reservar</Link>
+        </div>
 
       </div>
+
+
     </>
   )
 }
