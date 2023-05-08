4f8577c14af1e3d2eae497bbf595824d71e11e6d
mchajon-2021205 <mchajon-2021205@kinal.edu.gt>
2023-05-08 15:35:05 -0600

    Se creo componente para navBar

--- gestor-de-hoteles-react/src/App.jsx
+++ gestor-de-hoteles-react/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export const App = ()=> {
   const [count, setCount] = useState(0)
--- gestor-de-hoteles-react/src/components/NavBar.jsx
+++ gestor-de-hoteles-react/src/components/NavBar.jsx
@@ -1,30 +1,45 @@
 import React from 'react'
 import '../HomePage.css'
-import imagen from '../assets/react.png'
+import imagen from '../assets/logo.png'
 
 export const NavBar = () => {
   return (
     <>
-      <header>
-        <div className='contenedor'>
-          
-         <section id='logo'>
-            <img src={imagen} id='imagen' alt="" width={'50px'} height={'50px'}/>
-            <h1>Hotel</h1>
-         </section>
-          <div >
-            <nav>
-              <ul className='nav'>
-                <li>lista</li>
-                <li>list</li>
-                <li>listsa</li>
+      <header className="top-navbar">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="" style={{margin: 'auto', display: 'flex'}}>
+            <a className="navbar-brand" >
+              <img src={imagen} alt="" />
+            </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-rs-food" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbars-rs-food">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item active"><a className="nav-link" >Home</a></li>
+                <li className="nav-item"><a className="nav-link" >Menu</a></li>
+                <li className="nav-item"><a className="nav-link" >About</a></li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle"  id="dropdown-a" data-toggle="dropdown">Pages</a>
+                  <div className="dropdown-menu" aria-labelledby="dropdown-a">
+                    <a className="dropdown-item">Reservation</a>
+                    <a className="dropdown-item" >Stuff</a>
+                    <a className="dropdown-item" >Gallery</a>
+                  </div>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle"  id="dropdown-a" data-toggle="dropdown">Blog</a>
+                  <div className="dropdown-menu" aria-labelledby="dropdown-a">
+                    <a className="dropdown-item" >blog</a>
+                    <a className="dropdown-item" >blog Single</a>
+                  </div>
+                </li>
+                <li className="nav-item"><a className="nav-link" >Contact</a></li>
               </ul>
-            </nav>
+            </div>
           </div>
-        </div>
+        </nav>
       </header>
-    
-
     </>
   )
 }
