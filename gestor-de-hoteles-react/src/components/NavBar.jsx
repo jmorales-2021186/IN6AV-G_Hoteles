d5c43f8e4df07e1f74178f30c78c5bcef73e3f9f
mhurtado2021407 <mhurtado-2021407@kinal.edu.gt>
2023-05-15 16:21:03 -0600

    back antiguo deleted

--- gestor-de-hoteles-react/src/components/NavBar.jsx
+++ gestor-de-hoteles-react/src/components/NavBar.jsx
@@ -1,46 +1,76 @@
-import React from 'react'
-import '../HomePage.css'
-import imagen from '../assets/logo.png'
+import React from "react";
+import "../HomePage.css";
+import imagen from "../assets/logo.png";
 
 export const NavBar = () => {
   return (
     <>
-      <header className="top-navbar" >
-        <nav className="navbar navbar-expand-lg navbar-light bg-light" >
-          <div className="" style={{ width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <a className="navbar-brand" style={{ marginLeft: '20px' }}>
+      <header className="top-navbar">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div
+            className=""
+            style={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            {/* adsfa */}
+            <a className="navbar-brand" style={{ marginLeft: "20px" }}>
               <img src={imagen} alt="" />
             </a>
             {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-rs-food" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button> */}
-            <div className="" id="navbars-rs-food" style={{ marginRight: '120px' }}>
-<<<<<<< HEAD
-              <ul className="navbar-nav ml-auto " style={{marginLeft: '10px'}}>
-=======
-              <ul className="navbar-nav ml-auto">
->>>>>>> msicajan
-                <li className="nav-item active"><a className="nav-link" >Home</a></li>
-                <li className="nav-item"><a className="nav-link" >Menu</a></li>
-                <li className="nav-item"><a className="nav-link" >About</a></li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link " id="dropdown-a" data-toggle="dropdown">Pages</a>
+            <div
+              className=""
+              id="navbars-rs-food"
+              style={{ marginRight: "120px" }}
+            >
+              <ul
+                className="navbar-nav ml-auto "
+                style={{ marginLeft: "10px" }}
+              >
+                <li className="nav-item active">
+                  <a className="nav-link">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link">Menu</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link">About</a>
                 </li>
                 <li className="nav-item dropdown">
-                  <a className="nav-link " id="dropdown-a" data-toggle="dropdown">Blog</a>
+                  <a
+                    className="nav-link "
+                    id="dropdown-a"
+                    data-toggle="dropdown"
+                  >
+                    Pages
+                  </a>
                 </li>
-
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link "
+                    id="dropdown-a"
+                    data-toggle="dropdown"
+                  >
+                    Blog
+                  </a>
+                </li>
               </ul>
             </div>
-            <div style={{ marginRight: '10px' }}>
+            <div style={{ marginRight: "10px" }}>
               <ul className="navbar-nav ml-auto">
-
-                <li className="nav-item"><a className="nav-link" >Contact</a></li>
+                <li className="nav-item">
+                  <a className="nav-link">Contact</a>
+                </li>
               </ul>
             </div>
           </div>
         </nav>
       </header>
     </>
-  )
-}
+  );
+};
--- gestor-de-hoteles-react/src/pages/HomePage.jsx
+++ gestor-de-hoteles-react/src/pages/HomePage.jsx
@@ -1,32 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { NavBar } from '../components/NavBar'
-import '../HomePage.css'
-import header from '../assets/header.jpg'
+import React from "react";
+import { Link } from "react-router-dom";
+import { NavBar } from "../components/NavBar";
+import "../HomePage.css";
+import header from "../assets/header.jpg";
 
 export const HomePage = () => {
   return (
     <>
-
       <NavBar />
 
-      <div className='hero'>
-       
-         <div className='reservar'>
-          
-          <p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
-
-        </div> 
-        
+      <div className="hero">
+        <div className="reservar">
+          <p>
+            <a class="btn btn-lg btn-circle btn-outline-new-white" href="#">
+              Reservation
+            </a>
+          </p>
+        </div>
       </div>
 
-<<<<<<< HEAD
-   {/*  <div className='hola'>
+      {/*  <div className='hola'>
       <h1>Hoteles</h1>
     </div> */}
-=======
->>>>>>> msicajan
-
     </>
-  )
-}
+  );
+};
