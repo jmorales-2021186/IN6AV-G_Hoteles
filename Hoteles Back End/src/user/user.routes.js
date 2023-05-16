68381665bf99cc3f018317373d59382997639a65
jmorales-2021186 <jmorales-2021186@kinal.edu.gt>
2023-05-16 15:26:31 -0600

    se agregaron funcionalidades

--- Hoteles Back End/src/user/user.routes.js
+++ Hoteles Back End/src/user/user.routes.js
@@ -17,6 +17,7 @@ api.post('/login', userController.login);
 api.post('/register', userController.register);
 api.post('/search/:id', userController.searchHotelAndVook);
 
+
 //===============================================Rutas solo de Admin=================
 api.put('/update/:id', userController.update);
 api.delete('/delete/:id', userController.delete);
@@ -29,7 +30,8 @@ api.get('/seeUsersAdmin', userController.seeRegisteredUsersAdminHotel)
 
 //===============================================Rutas solo de Admin_HOTEL=================
 //Agregar habitaciones a un hotel
-api.put('/addHotles/:id', userController.addRooms)
+api.put('/addRoomsHotel/:id', userController.addRooms)
+api.put('/addEvents/:id', userController.addEvents)
 
 module.exports = api;
 
