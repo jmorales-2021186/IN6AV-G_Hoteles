605df98500dfd395e43c40b9de2d70ddab4f7dae
jmorales-2021186 <jmorales-2021186@kinal.edu.gt>
2023-05-17 10:04:47 -0600

    se hicieron cambios

--- Hoteles Back End/index.js
+++ Hoteles Back End/index.js
@@ -3,11 +3,12 @@
 require('dotenv').config()
 const mongoConfig = require('./configs/mongo');
 const app = require('./configs/app');
-const { userDefault } = require('./src/user/user.controller');
+const { userDefault, userDefaultClient } = require('./src/user/user.controller');
 const { emptyService } = require('./src/AdittionalsHoteles/servicios.controller');
 const { addTypeEventDefaul, addTypeEvent } = require('./src/eventType/type.controller');
 const { addEventEventDefaul } = require('./src/event/event.controller');
 const { addRoomDefault } = require('./src/room/room.controller');
+const { createReservationDafault } = require('./src/reservation/reservation.controller');
 
 mongoConfig.connect();
 app.initServer();
@@ -16,3 +17,5 @@ emptyService();
 addEventEventDefaul()
 addTypeEventDefaul();
 addRoomDefault();
+userDefaultClient();
+createReservationDafault();
