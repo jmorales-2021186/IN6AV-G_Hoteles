3d3aece172e92a297d285008cdb535c25c7b83bc
jmorales-2021186 <jmorales-2021186@kinal.edu.gt>
2023-05-16 18:31:59 -0600

    s

--- Hoteles Back End/src/reservation/reservation.routes.js
+++ Hoteles Back End/src/reservation/reservation.routes.js
@@ -4,10 +4,6 @@ const express = require('express')
 const api = express.Router();
 const reservationController = require('./reservation.controller')
 
-<<<<<<< HEAD
-// api.post('/create', reservationController.createReservation)
-=======
->>>>>>> jmorales
 // api.get('/get', typeController.getTypes)
 // api.get('/get/:id', typeController.getType)
 api.delete('/delete/:id', reservationController.deleteReservation)
